48566e8bf78b51760f8052d5fb6f24cfb1b526e8
03bennej <jjrbenn@gmail.com>
2023-06-30 20:52:52 +0000

    src scripts

---
+++ src/sub_folder/example.py
@@ -0,0 +1,2 @@
+from src.sub_folder.file_1 import square
+from src.file_2 import read_csv_file
---
+++ src/sub_folder/file_1.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def square(array):
+    squared_array = np.square(array)
+    return squared_array
